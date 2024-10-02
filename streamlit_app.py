7762c5d2854f88b1bc4643dc2189292954f0c02f
Lang <93107429+GalangHanafi@users.noreply.github.com>
2024-10-02 05:09:25 +0000

    tugas 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Hello World 🐶")
+
+name = st.text_input("Name :")
+
+if(name):
+    st.write('Halo,', name)
+else:
+    st.warning("isi nama oi!!!")
